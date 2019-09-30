d52d028482ddd5e2cb813d2a8ff3161acd4f2ab4
Ramon Pinho <ramon.pinho@afil.io>
2019-09-29 21:44:52 -0300

    montando repositório

---
+++ js/scripts.js
@@ -0,0 +1,45 @@
+var totalSlides = 1;
+
+function mostrarSlide(slide) {
+  var i;
+  var slides = document.getElementsByClassName("slide");
+
+  if (slide > slides.length) {
+    totalSlides = 1
+  };
+
+  if (slide < 1) {
+    totalSlides = slides.length
+  };
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  };
+
+  slides[totalSlides-1].style.display = "block";
+}
+
+mostrarSlide(totalSlides);
+
+function mudaSlide(slide) {
+  mostrarSlide(totalSlides += slide);
+}
+
+var video = document.getElementById("video");
+var btn = document.getElementById("mostrarVideo");
+var fechar = document.getElementsByClassName("fechar")[0];
+
+btn.onclick = function(e) {
+  e.preventDefault();
+  video.style.display = "block";
+}
+
+fechar.onclick = function() {
+  video.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == video) {
+    video.style.display = "none";
+  }
+}
